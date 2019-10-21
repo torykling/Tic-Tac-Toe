e3cd3419a22d0b23659ab9ce530459ed3bfa7c1f
Tory Klingenstein <toryklingenstein@gmail.com>
2019-10-21 11:12:01 -0400

    remove event listener once square is clicked

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ let buttons = document.querySelectorAll("button");
 let turn = document.querySelector(".turn");
 turn.innerText = "Blue's turn"
 
+// this event listener alternates background colors of squares clicked to be red or blue
 for (let i=0; i<buttons.length; i++) {
-	buttons[i].addEventListener("click", function(e) {
+	buttons[i].addEventListener("click", function handler(e) {
 		e.preventDefault();
 		if (turn.innerText === "Blue's turn") {
 			buttons[i].classList.add("blue");
 			turn.innerText = "Red's turn";
+			e.target.removeEventListener(e.type, arguments.callee);
 		} else if (turn.innerText === "Red's turn") {
 			buttons[i].classList.add("red");
 			turn.innerText = "Blue's turn";
+			e.target.removeEventListener(e.type, arguments.callee);
 		}
 		
 	})
