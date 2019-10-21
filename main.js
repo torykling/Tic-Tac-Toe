34706283e6541286409e670204813cb68bd4053f
Tory Klingenstein <toryklingenstein@gmail.com>
2019-10-21 11:02:41 -0400

    alternate colors on click

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 let buttons = document.querySelectorAll("button");
+let turn = document.querySelector(".turn");
+turn.innerText = "Blue's turn"
 
 for (let i=0; i<buttons.length; i++) {
 	buttons[i].addEventListener("click", function(e) {
 		e.preventDefault();
-		buttons[i].style.backgroundColor = "blue";
+		if (turn.innerText === "Blue's turn") {
+			buttons[i].classList.add("blue");
+			turn.innerText = "Red's turn";
+		} else if (turn.innerText === "Red's turn") {
+			buttons[i].classList.add("red");
+			turn.innerText = "Blue's turn";
+		}
+		
 	})
 }
+
+// reset button clears board
+let reset = document.querySelector("form");
+reset.addEventListener("submit", function(e) {
+	e.preventDefault();
+	for (let i=0; i<buttons.length; i++) {
+		buttons[i].classList.remove("red");
+		buttons[i].classList.remove("blue");
+	}
+})
